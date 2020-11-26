c54bcc9280d9dc8122aac7fc61f73a3d78705a09
Yelizaveta Crofts <yscrofts@hotmail.com>
2020-11-26 10:06:13 +0100

    added ScrollView and Linking

--- App/screens/Options.js
+++ App/screens/Options.js
@@ -1,36 +1,45 @@
 import React from "react";
-import { View, SafeAreaView } from "react-native";
+import { View, SafeAreaView, ScrollView, Linking, Alert } from "react-native";
 import { Entypo } from "@expo/vector-icons";
 
 import colors from "../constants/colors";
 import { RowItem, RowSeparator } from "../components/RowItem";
 
+const openURL = (url) => {
+    return Linking.openURL(url).catch(() => {
+        Alert.alert("Sorry, something went wrong")
+    });
+    
+}
+
 export default () => {
     return(
-        <SafeAreaView>
-            <View>
-                <RowItem 
-                    rightIcon={<Entypo name="chevron-right" size={20} color={colors.blue} />} 
-                    text="Themes"
-                    onPress={() => alert("Pressed!")}
-                />
+        <SafeAreaView style={{ flex: 1, backgroundColor: "#a73" }}>
+            <ScrollView>
+                <View>
+                    <RowItem 
+                        rightIcon={<Entypo name="chevron-right" size={20} color={colors.blue} />} 
+                        text="Themes"
+                        onPress={() => alert("Pressed!")}
+                    />
 
-                <RowSeparator />
+                    <RowSeparator />
 
-                <RowItem 
-                    rightIcon={<Entypo name="export" size={20} color={colors.blue} />} 
-                    text="React Native Basics"
-                    onPress={() => alert("Pressed!")}
-                />
+                    <RowItem 
+                        rightIcon={<Entypo name="export" size={20} color={colors.blue} />} 
+                        text="React Native Basics"
+                        onPress={() => openURL("https://www.google.com")}
+                    />
 
-                <RowSeparator />
+                    <RowSeparator />
 
-                <RowItem 
-                    rightIcon={<Entypo name="export" size={20} color={colors.blue} />} 
-                    text="React Native By Example"
-                    onPress={() => alert("Pressed!")}
-                />
-            </View>
+                    <RowItem 
+                        rightIcon={<Entypo name="export" size={20} color={colors.blue} />} 
+                        text="React Native By Example"
+                        onPress={() => openURL("https://www.google.com")}
+                    />
+                </View>
+            </ScrollView>
         </SafeAreaView>
 )
 };
